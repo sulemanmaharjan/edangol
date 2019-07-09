8e74f7666af6fd3769edae19152ceb0312fe32b7
Evan Sir <dangolevan@gmail.com>
2019-07-09 13:31:49 +0545

    ghf

--- sort.php
+++ sort.php
@@ -1,9 +1,17 @@
 !
 <?php
 $names =array ( "deepa", "anita","rupa", "manisha");
-sort ($names);
-for ($i=0; $i<3 ;$i++)
+asort ($names);
+for ($i=0; $i<4 ;$i++)
 {
-    
+   // echo $names [$i];
+}
+
+$names= array ("nepal"=>"kathmandu", "kavre"=>"okilo","bhaktapur"=>"banepa");
+krsort($names);
+foreach ($names as $key=>$value) {
+echo ($value);
+
+
 }
 ?>
