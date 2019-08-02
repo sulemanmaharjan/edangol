909cf152f1a08694013fdc8fcd0e9af982f96d7b
evan dangol <dangolevan@gmail.com>
2019-08-02 13:11:09 +0545

    ewrfwe

---
+++ s1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- bootstrap css cdn -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- bootstrap js cdn -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+     <form action="" class="form bg-primaryblack text-danger m-5" method="post" >
+      CustomerName:<input type="text" name="CustomerName">
+      <br><br>
+      Country:<input type="text" name="Country">
+      <br><br>
+      <input type="submit" value="add">
+      </form>
+      <?php
+    if($_POST)
+    {
+    $con=new mysqli("localhost","root","","sdangat45");
+    $a=$_REQUEST["customerName"];
+    $b=$_REQUEST["country"];
+    $sql= "insert into customers(CustomerName,Country)values('$a','$b')";
+    $result=$con->query($sql);
+}
+    ?>
+</body>
+</html>
